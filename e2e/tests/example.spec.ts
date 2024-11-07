3f31d79e95ca25c38ef7a5ec29b48e2672e70d9c
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-11-07 10:26:38 +0100

    update ports for tests

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:5174/');
+  await page.goto('http://localhost:4173/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Vite \+ React \+ TS/);
 });
 
 test('working count button', async ({ page }) => {
-  await page.goto('http://localhost:5174/');
+  await page.goto('http://localhost:4173/');
 
   const button = page.locator('button');
 
